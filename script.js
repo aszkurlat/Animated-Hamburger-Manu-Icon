aa20fff50200725a64f697a6ac18c1749d63ffbd
aszkurlat <szkurlatagnieszka@gmail.com>
2020-11-05 14:08:49 +0100

    add script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const purpleBurger = document.querySelector('.purpleBurger');
+purpleBurger.addEventListener('click', function(){
+    purpleBurger.classList.toggle('active');
+})
+const pinkBurger = document.querySelector('.pinkBurger');
+pinkBurger.addEventListener('click', function(){
+    pinkBurger.classList.toggle('active');
+})
+const orangeBurger = document.querySelector('.orangeBurger');
+orangeBurger.addEventListener('click', function(){
+    orangeBurger.classList.toggle('active');
+})
+const yellowBurger = document.querySelector('.yellowBurger');
+yellowBurger.addEventListener('click', function(){
+    yellowBurger.classList.toggle('active');
+})
